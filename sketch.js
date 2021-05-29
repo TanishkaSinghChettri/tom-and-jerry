4c532708a1abe3e7e84f5edc2e15b3831886a9b0
TanishkaSinghChettri <81763046+TanishkaSinghChettri@users.noreply.github.com>
2021-05-29 21:00:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,47 @@
-var tom, jerry, bgImg;
-var tomImg1, mouseImg1, tomImg2,mouseImg2;
-
-
+var cat, catRunning ,catSad;
+var garden;
+var mouseStanding,mouseTeasing,mouseEnd;
+var bg , canvas;
 function preload() {
     //load the images here
-  bgImg= loadImage("images/garden.png")
-tomImg1= loadAnimation("images/cat1.png")
-jerryImg1=loadanimation("images/mouse1.png")
-tomImg2= loadAnimation("images/cat2.png","images/cat3.png")
-jerryImg3=loadanimation("images/mouse2.png","images/mouse3.png")
+    cat = loadImage("images/cat1.png");
+    catRunning = loadAnimation("images/cat2.png","images/cat3.png");
+    catSad = loadImage("images/cat4.png"); 
+    garden = loadImage("images/garden.png");
+    mouseStanding = loadImage("images/mouse1.png");
+    mouseTeasing = loadAnimation("images/mouse2.png","images/mouse3.png");
+    mouseEnd=loadImage("images/mouse4.png")
 }
 
 function setup(){
-    createCanvas(1000,800);
+    canvas=createCanvas(1000,800);
+    
+
+  cat1 = createSprite(870,600)
+  cat1.addImage("cat",cat); 
+  cat1.scale=0.2;
+
+  mouse1 = createSprite(90,600),
+  mouse1.addImage("mouseStanding",mouseStanding);
+  mouse1.scale=0.15;
+    
+   
     //create tom and jerry sprites here
-    tom= createSprite(350, 350, 50, 30);
-    jerry=createSprite (50,350,50,50)
+
 }
 
 function draw() {
 
-    background(255);
+    background(garden);
     //Write condition here to evalute if tom and jerry collide
-
+    if(cat1.x -mouse1.x<(cat1.width - mouse1.width)/2){ 
+        cat1.velocityX = 0;
+        mouse1.addImage("mouseEnd",mouseEnd); 
+        mouse1.changeAnimation("mouseEnd");
+        cat1.addAnimation("catsad",catSad);
+        cat1.changeAnimation("catsad")
+        cat1.x=300;
+    }
     drawSprites();
 }
 
@@ -30,6 +49,16 @@ function draw() {
 function keyPressed(){
 
   //For moving and changing animation write code here
+  if(keyDown("LEFT_ARROW")){
+    cat1.velocityX = -5; 
+    cat1.addAnimation("cat",catRunning);
+    cat1.changeAnimation("cat");
+    mouse1.addAnimation("mouse",mouseTeasing);
+    mouse1.changeAnimation("mouse")
+    cat1.scale=0.2;
+
+} 
+
 
 
 }
